4c0d245f6ab1e2721865613f787f0ab510486238
Abbas Sayed <imabbassayed@gmail.com>
2026-03-08 16:04:25 -0700

    Update projects

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -103,12 +103,7 @@ export const Projects = ({ projects }: { projects: ProjectsProps }) => {
                                   {project.title}
                                 </h3>
 
-                                <time
-                                  className="projects__item__card__date"
-                                  dateTime={project.startDate}
-                                >
-                                  {project.startDate}
-                                </time>
+                              
                               </div>
                             </div>
 
